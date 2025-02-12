14235638d99b18c18f24022882ad156d17b7ecb1
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2025-02-12 17:12:01 -0500

    Add files via upload

--- setup.bash
+++ setup.bash
@@ -1,7 +1,9 @@
-cd Desktop/Dashboard/Dashboard
+
+cd 
+cd Desktop/Dashboard
 git pull
 flutter build linux
-cd build/linux/x86/release/bundle
+cd build/linux/x64/release/bundle
 #temp
 cd lib
 mv libSDL3.so.0.1.5 libSDL3.so
