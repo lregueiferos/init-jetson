5bb4785320fb0e6d4041ad14335a869783d9e756
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2025-02-18 16:02:28 -0500

    Add files via upload

--- Flutter-PI.bash
+++ Flutter-PI.bash
@@ -1,3 +1,6 @@
+git clone --depth 1 https://github.com/ardera/flutter-engine-binaries-for-arm.git engine-binaries
+cd engine-binaries
+sudo ./install.sh
 # install system libraries 
 sudo apt install cmake libgl1-mesa-dev libgles2-mesa-dev libegl1-mesa-dev libdrm-dev libgbm-dev ttf-mscorefonts-installer fontconfig libsystemd-dev libinput-dev libudev-dev  libxkbcommon-dev
 sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-alsa
