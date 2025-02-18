ae91d88cd7fda11f258db5a33c0064ff2c4298e5
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2025-02-18 14:47:04 -0500

    Add files via upload

---
+++ Flutter-PI.bash
@@ -0,0 +1,16 @@
+# install system libraries 
+sudo apt install cmake libgl1-mesa-dev libgles2-mesa-dev libegl1-mesa-dev libdrm-dev libgbm-dev ttf-mscorefonts-installer fontconfig libsystemd-dev libinput-dev libudev-dev  libxkbcommon-dev
+sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-alsa
+sudo apt-get install xdg-user-dirs
+# install fonts
+sudo fc-cache
+#clone and compile Flutter-pi
+cd
+git clone --recursive https://github.com/ardera/flutter-pi
+cd flutter-pi
+mkdir build && cd build
+cmake ..
+make -j`nproc`
+sudo make install
+sudo systemctl set-default multi-user.target
+Flutter-pi ~/Desktop/Dashbaord
