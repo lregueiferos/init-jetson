eda548b6976a27f264ae6fa4426d6509c995c206
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2025-02-19 15:22:08 -0500

    Add files via upload

--- Xinit.sh
+++ Xinit.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-cd
-cd Desktop/Dashboard/build/linux/
-cd */release/bundle
-xinit ./rover_control_dashboard $* -- :0 vt$XDG_VTNR
+sudo apt-get install xorg
+sudo apt install xterm
+cd Desktop/Dashboard/build/linux/*/release/bundle
+xinit ./rover_control_dahsboard $* -- :0 vt$XDG_VTNR
