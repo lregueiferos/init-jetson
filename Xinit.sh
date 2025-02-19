cd427c8eb5263b4c16c34534038837597402155f
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2025-02-19 15:31:52 -0500

    Update Xinit.sh

--- Xinit.sh
+++ Xinit.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
-cd /etc/systemd/system
-file="Rover-INIT.Service"
-echo "Adding first line" > $file
-echo "[Unit]" > $file
-echo "Description=Reboot message systemd service. " >> $file
-echo "" >> $file
-echo "[Service]" >> $file
-echo "Type=simple" >> $file
-echo "ExecStart=/bin/bash /home/init-jetson/Xinit.sh" >> $file
-echo "" >> $file
-echo "[Install]" >> $file
-echo "WantedBy=multi-user.target" >> $file
-cat $file
-chmod 644 Rover-INIT.Service
-systemctl Rover-INIT.Service
 cd
 cd Desktop/Dashboard/build/linux/
 cd */release/bundle
