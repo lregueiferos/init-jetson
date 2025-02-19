5c6af2514f7c4d3ca93c1846978390c96e97beeb
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2025-02-19 14:47:08 -0500

    Update Xinit.bash

--- Xinit.bash
+++ Xinit.bash
@@ -1,4 +1,4 @@
 sudo apt-get install xorg
 sudo apt install xterm
 cd Desktop/Dashboard/build/linux/*/release/bundle
-xinit ./rover_control_dahsboard $* -- :0 vt$XDG_VTNR
+xinit ./rover_control_dashboard $* -- :0 vt$XDG_VTNR
