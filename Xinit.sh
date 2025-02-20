0244c5ab2ea7d7ddfd24588c2e841a6dcc2aa968
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2025-02-20 16:06:00 -0500

    Update Xinit.sh

--- Xinit.sh
+++ Xinit.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+### BEGIN INIT INFO
+# Provides:          rover_control_dashboard
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Starts the Rover Control Dashboard
+# Description:       Initializes the Rover Control Dashboard with X session.
+### END INIT INFO
 
 export DISPLAY=:0
 export XDG_SESSION_TYPE=x11
@@ -8,4 +17,4 @@ cd *
 cd release/bundle || exit
 
 
-xinit ./rover_control_dashboard -- :0 vt1
+exec xinit ./rover_control_dashboard -- :0 vt1
