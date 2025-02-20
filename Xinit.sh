28ba0f49f3a8ef9924257a52e51b45f930d284c1
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2025-02-20 15:42:28 -0500

    Update Xinit.sh

--- Xinit.sh
+++ Xinit.sh
@@ -7,10 +7,5 @@ cd ~/Desktop/Dashboard/build/linux
 cd *
 cd release/bundle || exit
 
-# Ensure X server is running before launching
-while ! pgrep Xorg > /dev/null; do
-    echo "Waiting for X server to start..."
-    sleep 2
-done
 
 xinit ./rover_control_dashboard -- :0 vt1
