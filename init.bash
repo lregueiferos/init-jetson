0bbf3e59977c5b99223fc02bb52856fc9c8bdd29
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2025-08-27 17:28:09 -0400

    remove pi setup

--- init.bash
+++ init.bash
@@ -11,7 +11,7 @@ if [[response == "N"]]
 fi 
 
 #checks the number of phisical cpu cores on the system
-if [[ $(nproc) -eq 6]]; then
+
 
     #updates the system
     sudo apt update
@@ -57,30 +57,4 @@ if [[ $(nproc) -eq 6]]; then
 
 # runs the pi setup if the number of cores is not 6 as the jetson has 6 cpu cores and the pi has 4
 # if future pi's have 6 cores instead evaluate the system based off of the userid
-else 
-    sudo apt update
-    sudo apt upgrade -y
-    sudo apt install curl
-    cd Desktop
-    git clone https://github.com/BinghamtonRover/Dashboard
-    git clone https://github.com/BinghamtonRover/Rover-Code
-    cd
-    cd Downloads
-    git clone https://github.com/NaiveInvestigator/flutter-installer
-    cd flutter-installer
-    bash install.sh
-    sudo rm -rf flutter-installer
-    cd
-    flutter doctor
 
-    # downlaods flutter pi and flutterpi-tool
-    sudo apt install cmake libgl1-mesa-dev libgles2-mesa-dev libegl1-mesa-dev libdrm-dev libgbm-dev ttf-mscorefonts-installer fontconfig libsystemd-dev libinput-dev libudev-dev  libxkbcommon-dev
-    sudo fc-cache
-    git clone --recursive https://github.com/ardera/flutter-pi
-    cd flutter-pi
-    mkdir build && cd build
-    cmake ..
-    make -j`nproc`
-    sudo make install
-    flutter pub global activate flutterpi_tool
-    reboot
