bbb797d85aac35457aaa62c8ba1137fa67ac7360
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2025-10-30 17:25:02 -0400

    Fix directory paths and improve script flow
    Updated directory paths and added exit command.

--- setup.bash
+++ setup.bash
@@ -1,17 +1,12 @@
 
 cd 
-cd Desktop/Dashboard
+cd /home/rover/Desktop/Dashboard
 git pull
 flutter build linux
-cd build/linux/x64/release/bundle
-#temp
-cd lib
-mv libSDL3.so.0.1.5 libSDL3.so
-cd ..
 #temp
 cd 
-cd Desktop/Rover-Code
+cd /home/rover/Desktop/Rover-Code
 git submodule update --init
 git pull
-dart run 
-cd
+dart run
+exit
